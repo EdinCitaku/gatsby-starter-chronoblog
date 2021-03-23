63f6e5f8311af7eb3ba7b14cfaff2ec826f1d9f9
edin citaku <edin.citaku@jetbrains.com>
2021-03-24 00:20:19 +0100

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
           resolve: `gatsby-plugin-google-analytics`,
           options: {
               // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-              trackingId: 'UA-XXXXXXXXX-X'
+              trackingId: 'G-5DCMBLKKPK'
           }
       }
   ]
